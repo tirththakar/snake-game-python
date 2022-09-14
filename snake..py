0b0029fdf234a7f623004d7b37e28eb17ad95e06
TirthT <tirth.thakar174@gmail.com>
2022-09-14 17:22:55 +1200

    Fixed the bug where hitting the boundary was not being detected. Also added the option to restart the game if a player wants.
    Made minor adjustments to the text alignment of the end message

--- snake..py
+++ snake..py
@@ -11,7 +11,9 @@ snake_colour = (0, 0, 255)
 interface_width = 500
 interface_height = 500
 snake_speed = 15
-snake_dimension = 10
+snake_dimension = 15
+game_finished = False
+close_game = False
 # Setting up interface for the game using pygame built-in functions
 game_interface = pygame.display.set_mode((interface_width, interface_height))
 pygame.display.update()
@@ -51,33 +53,45 @@ def new_game_or_close():
 
 def hit_boundary(x_pos, y_pos):
     global close_game
+    global interface_height
+    global interface_width
     if x_pos >= interface_width or x_pos < 0 or y_pos >= interface_height or y_pos < 0:
         close_game = True
 
 def ate_food(x_pos, y_pos, food_x, food_y):
+    global interface_height
+    global interface_width
+    global snake_dimension
     if x_pos == food_x and y_pos == food_y:
-        food_x = round(random.randrange(0, interface_width - snake_dimension))
-        food_y = round(random.randrange(0, interface_height - snake_dimension))
+        print([x_pos, y_pos, food_x, food_y])
+        food_x = round(random.randrange(0, interface_width - snake_dimension) / 10.0) * 10.0
+        food_y = round(random.randrange(0, interface_height - snake_dimension) / 10.0) * 10.0
     return food_x, food_y
 
 def display_message(message, colour):
-    msg = pygame.font.SysFont(None, 50).render(message, True, colour)
-    game_interface.blit(msg, [interface_width/3, interface_height/3])
+    msg = pygame.font.SysFont(None, 25).render(message, True, colour)
+    game_interface.blit(msg, [interface_width/9, interface_height/3])
 
 clock = pygame.time.Clock()
 
 def play():
     # Variable to keep track of whether or not game has ended or not
-    game_finished = False
+    global interface_height
+    global interface_width
+    global snake_dimension
+    global close_game
+    global game_finished
+
     close_game = False
+    game_finished = False
 
     x_cord_change = 0
     y_cord_change = 0
     current_position = [interface_width/2, interface_height/2, snake_dimension, snake_dimension]
 
     # Loop to keep playing game while it has not ended and keep track of events
-    food_x = round(random.randrange(0, interface_width - snake_dimension))
-    food_y = round(random.randrange(0, interface_height - snake_dimension))
+    food_x = round(random.randrange(0, interface_width - snake_dimension) / 10.0) * 10.0
+    food_y = round(random.randrange(0, interface_height - snake_dimension) / 10.0) * 10.0
     while not game_finished:
 
         while close_game == True:
@@ -87,9 +101,9 @@ def play():
             new_game_or_close()
 
         x_cord_change, y_cord_change = movement_events(x_cord_change, y_cord_change)
+        hit_boundary(current_position[0], current_position[1])
         current_position[0] += x_cord_change
         current_position[1] += y_cord_change
-        hit_boundary(current_position[0], current_position[1])
         game_interface.fill(background_colour)
         pygame.draw.circle(game_interface, (0, 0, 0), [food_x, food_y], 5)
         pygame.draw.rect(game_interface, snake_colour, current_position)
